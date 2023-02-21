2250476b8cf59be3aa49fc71c179b3c0cbe8a167
Eduardo Lopez <lalolt32@hotmail.com>
2023-02-20 20:28:55 -0600

    Implementacion de react redux

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,16 +1,25 @@
 import React from "react";
-import products from "../data/products.js";
 import { StyleSheet, Image, FlatList, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
+import { useSelector, useDispatch } from "react-redux";
+import { productsSlice } from "../store/productsSlice";
+
 const ProductsScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const products = useSelector((state) => state.products.products);
   return (
     <FlatList
       data={products}
       renderItem={({ item }) => (
         <Pressable
-          onPress={() => navigation.navigate("Product Details")}
+          onPress={() => {
+            //Update selected product
+            dispatch(productsSlice.actions.setSelectedProduct(item.id));
+            navigation.navigate("Product Details");
+          }}
           style={styles.itemContainer}
         >
           <Image source={{ uri: item.image }} style={styles.image} />
